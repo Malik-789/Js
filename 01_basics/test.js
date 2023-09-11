3a2261e9760e494de974b11a3522e575a10efab8
Malik-789 <144707536+Malik-789@users.noreply.github.com>
2023-09-11 17:06:40 +0000

    change folder name

---
+++ 01_basics/test.js
@@ -0,0 +1,16 @@
+console.log("Malik")
+const accounID = 12345
+let accoundEmail = "malik@gmail.com"
+var accountPassword = "hehe"
+accountCity = "FSD"
+
+let accountNamjje;
+
+accoundEmail = "abc@gmail.com"
+accountPassword = "motorbike"
+accountCity = "LHR"
+
+
+console.log(accounID);
+
+console.table([accounID, accoundEmail, accountPassword, accountCity, accountNamjje])
